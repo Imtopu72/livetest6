348f9559fd07a3271d00695ae47e7b66508b9cfa
Imtopu72 <abstopu39@gmail.com>
2023-06-12 23:33:19 +0600

    Live Test 6

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shopping List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Shopping(),
+    );
+  }
+}
+
+class Shopping extends StatelessWidget {
+  final List<String> shoppingItems = [
+    'Apples',
+    'Bananas',
+    'Bread',
+    'Milk',
+    'Eggs',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center (
+        child: Text ('My Shopping List'),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Cart is empty'),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text(shoppingItems[index]),
+          );
+        },
+      ),
+    );
+  }
+}
